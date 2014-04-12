7d01edcce7fff6bf9a5e1f749457449fe32e4982
Daniel Codrea <daniel.codrea@inadcod.com>
2014-04-12 09:25:01 +0300

    english version

--- en/contact.php
+++ en/contact.php
@@ -28,7 +28,7 @@ if(isset($_POST['submit'])) {
             $comments = trim($_POST['comments']);
         }
     }
-    
+
     if(trim($_POST['subject']) == '') {
         $subject = "Formular de contact vilagong.ro";
     } else {
@@ -52,11 +52,11 @@ if(isset($_POST['submit'])) {
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Vila Gong | Contact</title>
-    <link rel="stylesheet" href="css/foundation.css" />
-    <link href="css/colorbox.css" rel="stylesheet" />
-      <link href="css/font-awesome.css" rel="stylesheet" />
+    <link rel="stylesheet" href="../css/foundation.css" />
+    <link href="../css/colorbox.css" rel="stylesheet" />
+      <link href="../css/font-awesome.css" rel="stylesheet" />
 
-    <script src="js/vendor/modernizr.js"></script>
+    <script src="../js/vendor/modernizr.js"></script>
   </head>
   <body>
       <div class="row collapse">
@@ -64,51 +64,51 @@ if(isset($_POST['submit'])) {
               <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d87485.6226747592!2d23.388431!3d46.74664699999999!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47491ad95cca95e1%3A0xf3966c6135ac4a16!2sVila+Gong!5e0!3m2!1sen!2s!4v1396411873804" width="940" height="400" frameborder="0" style="border:0; margin-bottom:-2px;"></iframe>
           </div>
       </div>
-    
+
     <div id="siteTop">
          <div class="row">
             <div class="small-12 columns">
                 <div id="logo" class="left">
                     <a href="index.html">
-                        <img src="img/logo.png" alt="Vila Gong" />
+                        <img src="../img/logo.png" alt="Vila Gong" />
                     </a>
                 </div>
                 <div id="lang" class="right">
                     <ul class="inline-list">
-                        <li><a href="contact.php"><img src="img/Romania.png" alt="Romnian" /></a></li>
-                        <li><a href="en/contact.php"><img src="img/United-Kingdom.png" alt="English" /></a></li>
-                        <li><a href="hu/kapcsolat.php"><img src="img/Hungary.png" alt="Hungarian" /></a></li>
+                        <li><a href="../contact.php"><img src="../img/Romania.png" alt="Romnian" /></a></li>
+                        <li><a href="contact/php"><img src="../img/United-Kingdom.png" alt="English" /></a></li>
+                        <li><a href="../hu/kapcsolat.php"><img src="../img/Hungary.png" alt="Hungarian" /></a></li>
                     </ul>
                 </div>
                 <div class="clearfix"></div>
             </div>
         </div>
         <div id="bookings">
-            <p class="semibolditalic">Rezervari online</p>
+            <p class="semibolditalic">Online booking</p>
         </div>
     </div>
-      
-      
-      
-      
+
+
+
+
       <div id="menu" class="row">
         <div class="small-12 columns">
             <nav>
                 <ul class="inline-list">
                     <li><a href="index.html">Home</a></li>
-                    <li class="pozRel"><a href="#">Facilitati <i class="fa fa-caret-down fa-lg mL5"></i></a>
+                    <li class="pozRel"><a href="#">Facilities <i class="fa fa-caret-down fa-lg mL5"></i></a>
                         <ul class="sub-menu">
-                            <li><a href="camere.html">Camere</a></li>
+                            <li><a href="rooms.html">Rooms</a></li>
                             <li><a href="restaurant.html">Restaurant</a></li>
-                            <li><a href="sala_conferinte.html">Sala de Conferinte</a></li>
-                        </ul>    
+                            <li><a href="conference_hall.html">Conference Hall</a></li>
+                        </ul>
                     </li>
-                    <li><a href="galerie_foto.html">Galerie Foto</a></li>
-                    <li><a href="oferte.html">Oferte</a></li>
-                    <li><a href="atractii.html">Atractii</a></li>
+                    <li><a href="photo_gallery.html">Photo Gallery</a></li>
+                    <li><a href="offers.html">Offers</a></li>
+                    <li><a href="sightseeing.html">Sightseeing</a></li>
                     <li><a class="active" href="contact.php">Contact</a></li>
                 </ul>
-            </nav>  
+            </nav>
         </div>
     </div>
     <p class="spacer20">&nbsp;</p>
@@ -118,62 +118,62 @@ if(isset($_POST['submit'])) {
                 <div class="small-6 columns">
                     <div class="p20">
                         <p class="spacer20">&nbsp;</p>
-                        <p>Adresa: 407310 Gilău, Str. Someşul Rece, Nr. 1153/C, Jud. Cluj</p>
-                        <p>Telefon: +40-264-371468, +40-723-193977</p>
+                        <p>Address: 407310 Gilău, 1153/C Someşul Rece street, Cluj county</p>
+                        <p>Phone: +40-264-371468, +40-723-193977</p>
                         <p>Fax: +40-264-371498</p>
                         <p>Email: info@vilagong.ro</p>
                         <p>Web: http://www.vilagong.ro/</p>
-                        <p class="small"><span class="semibold">SC Rentaporta Import Export SRL</span>, Nr. RC: J12/2665/1993, Cod fiscal: 4341094</p>
+                        <p class="small"><span class="semibold">SC Rentaporta Import Export SRL</span>, VAT: J12/2665/1993, Fiscal code: 4341094</p>
                         <p class="spacer20">&nbsp;</p>
                     </div>
                 </div>
                 <div class="small-6 columns">
                     <div class="p20">
-                        <h4>Lasati-ne un mesaj</h4>
+                        <h4>Leave us a message</h4>
                         <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" id="contactForm">
-                            <input type="text" name="name" placeholder="Nume" />
+                            <input type="text" name="name" placeholder="Name" />
                             <input type="email" name="email" placeholder="Email" />
-                            <input type="text" name="subject" placeholder="Subiect" />
-                            <textarea name="comments" placeholder="Mesaj"></textarea>
-                            <input type="submit" class="button radius tiny" name="submit" value="Trimite" />
+                            <input type="text" name="subject" placeholder="Subject" />
+                            <textarea name="comments" placeholder="Message"></textarea>
+                            <input type="submit" class="button radius tiny" name="submit" value="Send" />
                         </form>
                         <?php if(isset($hasError)) { //If errors are found ?>
-                            <p class="error"><b>Eroare!</b></p>
-                            <p class="error">Va rugam sa verificati corectitudinea informatiilor din fiecare camp obligatoriu al formularului. Va multumim!</p>
+                            <p class="error"><b>Error!</b></p>
+                            <p class="error">Please check that you have correctly filled in all the mandatory fields. Thank you!</p>
                         <?php } ?>
                         <?php if(isset($emailSent) && $emailSent == true) { //If email is sent ?>
-                            <p><b>Mesajul a fost trimis cu succes!</b></p>
-                            <p>Multumim <b><?php echo $name;?></b> pentru mesajul tau! Vom reveni cu un raspuns in cel mai scurt timp posibil.</p>
+                            <p><b>Message sent successfully!</b></p>
+                            <p>Thank you <b><?php echo $name;?></b> for your message! We will get back to you as soon as possible.</p>
                         <?php } ?>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-      
+
     <p class="spacer20">&nbsp;</p>
-      
+
     <footer>
-      
+
         <div class="row">
             <div class="small-6 columns">
-                <p class="small">&copy <span id="currentYear"></span> Vila Gong. Toate dreturile rezervate.</p>
-            </div>  
+                <p class="small">&copy <span id="currentYear"></span> Vila Gong. All rights reserved.</p>
+            </div>
             <div class="small-6 columns">
                 <p class="small textRight">Design by: <a href="http://inadcod.com">inadcodDesign</a>. Powered by: <a href="http://seewebsolutions.ro">See-Web Solutions</a></p>
             </div>
         </div>
-        
+
     </footer>
- 
 
-    	
 
-    
-    <script src="js/vendor/jquery.js"></script>
-    <script src="js/foundation.min.js"></script>  
-    <script src="js/bjqs-1.3.min.js"></script>  
-    <script src="js/jquery.colorbox-min.js"></script>
+
+
+
+    <script src="../js/vendor/jquery.js"></script>
+    <script src="../js/foundation.min.js"></script>
+    <script src="../js/bjqs-1.3.min.js"></script>
+    <script src="../js/jquery.colorbox-min.js"></script>
     <script>
         $(document).foundation();
         $(window).load(function(){
