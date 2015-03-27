c6a7076ce919c701ba6c67950cb279e3dd1a5470
Daniel Codrea <daniel.codrea@gmail.com>
2015-03-27 22:22:24 +0200

    Fixed small js errors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,25 @@
 'use strict';
 
 $('#homepage-carousel').carousel({
-    pause: "false",
-    interval: 400000
+    pause: false,
+    interval: 4000
 });
 
-$('#homepage-carousel .item').css({'position': 'absolute', 'width': '100%', 'height': '100%'});
 $('#homepage-carousel .carousel-inner div.item img').each(function() {
   var imgSrc = $(this).attr('src');
-  $(this).next('.image-container').css({'position': 'absolute', 'width': '100%', 'height': '100%', 'background': 'url('+imgSrc+') center center no-repeat', '-webkit-background-size': '100% ', '-moz-background-size': '100%', '-o-background-size': '100%', 'background-size': '100%', '-webkit-background-size': 'cover', '-moz-background-size': 'cover', '-o-background-size': 'cover', 'background-size': 'cover', 'z-index': '10'});
+  $(this).next('.image-container').css({
+    'position': 'absolute', 
+    'width': '100%', 
+    'height': '100%', 
+    'background': 'url('+imgSrc+') center center no-repeat', 
+    'background-size': 'cover', 
+    'z-index': '10'
+  });
   $(this).remove();
 });
 
-$(window).on('resize', function() {
-  $('#homepage-carousel').css({'width': '100%', 'height': '100%'});
-});
-
 $(function() {
-    $('a[href*=#]:not([href=#], .noJumpLink)').click(function() {
+    $('a.jumpLink').click(function() {
         if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -29,8 +31,4 @@ $(function() {
             }
         }
     });
-  $('.nav a').on('click', function(){
-    $('.nav li').removeClass('active');
-    $(this).parent().addClass('active');
-  });
 });
